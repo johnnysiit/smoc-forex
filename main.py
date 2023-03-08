04a2336e95a40a3d80e95cc95071b0f9842527bf
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2023-03-08 15:04:28 -0600

    Strategy Test Update

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ import os
 import time
 import matplotlib.pyplot as plt
 
-def get_data_1m(ties):
-    data = yf.download(tickers = (ties+'=X') ,period ='2d', interval = '1m')
+def get_data(pair,period,interval):
+    data = yf.download(tickers = (pair+'=X') ,period =period, interval = interval)
     data.index = pd.to_datetime(data.index)
     data = data.sort_index(ascending=True)
     return data
 
-def get_data_1h(ties):
-    data = yf.download(tickers = (ties+'=X') ,period ='5d', interval = '1h')
-    data.index = pd.to_datetime(data.index)
-    data = data.sort_index(ascending=True)
-    return data
 
 def diff_strategy_making(data):
     ##DIFF
@@ -95,20 +90,28 @@ def trd_graph(data):
 
 def sender(text,graph,api_key):
     chat_id = '-1001829636788'
-    file = open(graph, 'rb')
-    api_url = f"https://api.telegram.org/bot{api_key}/"
-    method = "sendPhoto"
-    params = {'chat_id': chat_id}
-    files = {'photo': file}
-    caption = text
-    resp = requests.post(api_url + method, params=params, files=files, data={'caption': caption})
+    
+    #######Send Graph and text
+    # file = open(graph, 'rb')
+    # api_url = f"https://api.telegram.org/bot{api_key}/"
+    # method = "sendPhoto"
+    # params = {'chat_id': chat_id}
+    # files = {'photo': file}
+    # caption = text
+    # resp = requests.post(api_url + method, params=params, files=files, data={'caption': caption})
+    # print(resp)
+
+    #####send text
+    url = "https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={text}"
+    url = url.format(api_key=api_key, chat_id=chat_id, text=text)
+    resp = requests.get(url)
     print(resp)
 
-def diff_sender(data,graph,ties,api_key):
+def diff_sender(data,graph,pairs,api_key):
     #create a file if not exist
     if not os.path.exists("diff_direction.config"):
         config = open("diff_direction.config", "w")
-        config.write(ties + ":notset\n")
+        config.write(pairs + ":notset\n")
         config.close()
 
     config = open("diff_direction.config", 'r')
@@ -119,16 +122,16 @@ def diff_sender(data,graph,ties,api_key):
         config_dict[key] = value
     config.close()
 
-    if not ties in config_dict:
-        config_dict[ties] = 'notset'
+    if not pairs in config_dict:
+        config_dict[pairs] = 'notset'
 
     #if data['signal'] in last two rows are different, print('buy')
-    if data['signal'].iloc[-1] != config_dict[ties]:
-        signal = ('[' + ties + '] Signal changed to '+ data['signal'].iloc[-1])
+    if data['signal'].iloc[-1] != config_dict[pairs]:
+        signal = ('[' + pairs + '] DIFF Signal changed to '+ data['signal'].iloc[-1])
         print(signal)
         sender(signal,graph,api_key)
 
-        config_dict[ties] = data['signal'].iloc[-1]
+        config_dict[pairs] = data['signal'].iloc[-1]
         config = open("diff_direction.config", "w")
         config.truncate(0)
         for key, value in config_dict.items():
@@ -136,10 +139,10 @@ def diff_sender(data,graph,ties,api_key):
         config.close()
 
 
-def trd_sender(data,graph,ties,api_key):
+def trd_sender(data,graph,pairs,api_key):
     #create a file if not exist
-    pair_signal_name = ties + "_signal"
-    pair_position = ties+'_position'
+    pair_signal_name = pairs + "_signal"
+    pair_position = pairs+'_position'
     if not os.path.exists("trd_direction.config"):
         config = open("trd_direction.config", "w")
         config.write(pair_signal_name + ":notset\n")
@@ -158,25 +161,14 @@ def trd_sender(data,graph,ties,api_key):
     if not pair_position in config_dict:
         config_dict[pair_position] = 'notset'
 
-    #########signal
-    if data['trd_signal'].iloc[-1] != config_dict[pair_signal_name]:
-        signal = ('[' + ties + '] TRD Signal changed to '+ data['trd_signal'].iloc[-1])
-        sender(signal,graph,api_key)
-        config_dict[pair_signal_name] = data['trd_signal'].iloc[-1]
-        config = open("trd_direction.config", "w")
-        config.truncate(0)
-        for key, value in config_dict.items():
-            config.write(key + ":" + value + "\n")
-        config.close()
-
-    ###########gup
+    ###########gup###############
     if data['bb'].iloc[-1] < 25 and config_dict[pair_position] != 'low':
         position = 'low'
-        text = ('[' + ties + '] TRD Drop below 25')
+        text = ('[' + pairs + '] TRD Drop below 25')
         sender(text,graph,api_key)
     elif data['gup27'].iloc[-1] > 75 and config_dict[pair_position] != 'high':
         position = 'high'
-        text = ('[' + ties + '] TRD Rise above 75')
+        text = ('[' + pairs + '] TRD Rise above 75')
         sender(text,graph,api_key)
     elif data['bb'].iloc[-1] > 25 and data['gup27'].iloc[-1] < 75 and config_dict[pair_position] != 'mid':
         position = 'mid'
@@ -189,6 +181,41 @@ def trd_sender(data,graph,ties,api_key):
     for key, value in config_dict.items():
       config.write(key + ":" + value + "\n")
     config.close()
+    #############################
+
+def double_sender(data,graph,pairs,api_key):
+    # if trd_signal = buy, and diff_signal = buy, set b_signal to buy
+    data['b_signal'] = data.apply(lambda x: 'Buy' if x['trd_signal'] == 'Buy' and x['signal'] == 'Buy' else 'Sell' if x['trd_signal'] == 'Sell' and x['signal'] == 'Sell' else 'notset', axis=1)
+    #create a file if not exist
+    pair_signal_name = pairs + "_signal"
+    pair_position = pairs+'_position'
+    if not os.path.exists("both_direction.config"):
+        config = open("both_direction.config", "w")
+        config.write(pair_signal_name + ":notset\n")
+        config.close()
+    
+    config = open("both_direction.config", 'r')
+    config_dict = {}
+    config_lines = config.readlines()
+    for line in config_lines:
+        key, value = line.strip().split(':')
+        config_dict[key] = str(value)
+    config.close()
+
+    if not pair_signal_name in config_dict:
+        config_dict[pair_signal_name] = 'notset'
+    
+    #########signal
+    if (data['b_signal'].iloc[-1] != config_dict[pair_signal_name]) and (data['b_signal'].iloc[-1] != 'notset'):
+        trd_average = round(data['bb'].iloc[-1] + data['gup27'].iloc[-1])/2
+        signal = ('[' + pairs + '] BOTH Signal changed to '+ data['b_signal'].iloc[-1] + '\nCurrent TRD Position: '+ str(trd_average))
+        sender(signal,graph,api_key)
+    config_dict[pair_signal_name] = data['b_signal'].iloc[-1]
+    config = open("both_direction.config", "w")
+    config.truncate(0)
+    for key, value in config_dict.items():
+        config.write(key + ":" + value + "\n")
+    config.close()
 
 
 def trade():
@@ -196,40 +223,39 @@ def trade():
 
 if __name__ == '__main__':
     api = ''
-    while True:
-        #ties_list = ['USDJPY','USDCAD','AUDUSD','EURUSD']
-        ties_list = ['EURUSD','USDJPY']
-        for ties in ties_list:
-            print('start scraping ' + ties)
-            try:
-                onem_data = get_data_1m(ties)
-                #oneh_data = get_data_1h(ties)
-                oneh_data = onem_data 
-                print('Data collection success')
-            except:
-                print('Data collection error')
-                continue
-            try:
-                onem_data = diff_strategy_making(onem_data)
-                oneh_data = trd_strategy_making(oneh_data)
-                print(onem_data)
-            except:
-                print('data process failed')
-            try:
-                diff_graph_file = diff_graph(onem_data)
-                trd_graph_file = trd_graph(oneh_data)
-                print('Graph creation success')
-            except:
-                print('Graph creation error')
-                continue
-            try:
-                diff_sender(onem_data,diff_graph_file,ties,api)
-                trd_sender(oneh_data,trd_graph_file,ties,api)
-                print('Telegram message success')
-            except:
-                print('Telegram message error')
-                continue
-            time.sleep(10)
-        #delete graphs folder
+    # while True:
+    #pairs_list = ['USDJPY','USDCAD','AUDUSD','EURUSD']
+    pairs_list = ['EURUSD','AUDUSD']
+    for pairs in pairs_list:
+        print('start scraping ' + pairs)
+        try:
+            fx_data = get_data(pairs,'2d','1m')
+            print('Data collection success')
+        except:
+            print('Data collection error')
+            continue
+        try:
+            fx_data = diff_strategy_making(fx_data)
+            fx_data = trd_strategy_making(fx_data)
+            print(fx_data)
+        except:
+            print('data process failed')
+        try:
+            diff_graph_file = diff_graph(fx_data)
+            trd_graph_file = trd_graph(fx_data)
+            print('Graph creation success')
+        except:
+            print('Graph creation error')
+            continue
+        try:
+            diff_sender(fx_data,diff_graph_file,pairs,api)
+            #trd_sender(fx_data,trd_graph_file,pairs,api)
+            double_sender(fx_data,trd_graph_file,pairs,api)
+            print('Telegram message success')
+        except:
+            print('Telegram message error')
+            continue
+        time.sleep(10)
+    #delete graphs folder
         os.system('rm -rf graphs')
-        time.sleep(50)
+        # time.sleep(50)
--- strategy_test.py
+++ strategy_test.py
@@ -1,41 +1,88 @@
 import main as m
 import pandas as pd
-
-data = m.get_data('EURUSD','1wk','1m')
+import matplotlib.pyplot as plt
+import price_list as pl
+ticker = 'IQ'
+interval = '1h'
+period = 360
+data = pl.get_table(ticker,interval,period)
 data.index = pd.to_datetime(data.index)
 data = data.sort_index(ascending=True)
 data = m.diff_strategy_making(data)
 data = m.trd_strategy_making(data)
-print(data)
-balance = 10000
+balance = data['Close'].iloc[0]
 position = 0
 direction = 'notset'
 data['balance'] = 0
+data['posit'] = 0
+open_price = 0
+data['open_price'] = 0
+data['value'] = 0
+#open_price = data['Close'].iloc[0]
 for i in range(len(data)):
     if data['signal'].iloc[i] == 'Buy' and direction != 'Buy' and data['trd_signal'].iloc[i] == 'Buy':
         direction = 'Buy'
-        #close short position
-        balance = balance + abs(position*data['Close'].iloc[i])
-        #data[balance] = balance
-        data['balance'].iloc[i] = balance
-        print('balance: ' + str(balance))
+        balance = balance + (position*open_price*(-1)) + (position*(-1)*(open_price - data['Close'].iloc[i]))
         position = balance/data['Close'].iloc[i]
+        data['posit'].iloc[i] = position
         balance = 0
+        open_price = float(data['Close'].iloc[i])
+        data['open_price'].iloc[i] = open_price
+
     elif data['signal'].iloc[i] == 'Sell' and direction != 'Sell' and data['trd_signal'].iloc[i] == 'Sell':
         direction = 'Sell'
-        balance = balance + abs(position*data['Close'].iloc[i])
-        data['balance'].iloc[i] = balance
-        print('balance: ' + str(balance))
-        position = (balance/data['Close'].iloc[i])*-1
+        #get the balance from the position and last oepn price
+        balance = balance + position*open_price + position*(data['Close'].iloc[i] - open_price)
+        position = balance/data['Close'].iloc[i]*-1
+        data['posit'].iloc[i] = position
         balance = 0
-    # elif data['signal'].iloc[i] != data['trd_signal'].iloc[i] and direction != 'Hold':
-    #     direction = 'Hold'
-    #     balance = balance + abs((position*data['Close'].iloc[i]))
-    #     #data['balance'].iloc[i] = balance
-    #     print('balance: ' + str(balance))
-    #     position = 0
+        open_price = float(data['Close'].iloc[i])
+        data['open_price'].iloc[i] = open_price
+
+    elif data['signal'].iloc[i] != data['trd_signal'].iloc[i] and direction != 'Hold':
+        direction = 'Hold'
+        if data['posit'].iloc[i-1] > 0:
+            balance = balance + position*open_price + position*(data['Close'].iloc[i] - open_price)
+        elif data['posit'].iloc[i-1] < 0:
+            balance = (balance + position*open_price*-1) + (position*(open_price - data['Close'].iloc[i])*-1)
+        
+        data['balance'].iloc[i] = balance
+        position = 0
+        data['posit'].iloc[i] = position
+        open_price = 0
+        data['open_price'].iloc[i] = open_price
+    else:
+        data['posit'].iloc[i] = position
+        data['balance'].iloc[i] = balance
+        data['open_price'].iloc[i] = open_price
+
+
+
  #output data to excel
-data.to_excel('output.xlsx')
-if balance == 0:
-    balance = abs((position*data['Close'].iloc[-1])*1)
-print('balance: ' + str(balance))
+for i in range(len(data)):
+    balance = data['balance'].iloc[i]
+    position = data['posit'].iloc[i]
+    open_price = data['open_price'].iloc[i]
+    if data['posit'].iloc[i] > 0:
+        data['value'].iloc[i] = balance + position*open_price + position*(data['Close'].iloc[i] - open_price)
+    elif data['posit'].iloc[i] < 0:
+        data['value'].iloc[i] = balance + (position*open_price*-1) + (position*(open_price - data['Close'].iloc[i])*-1)
+    else:
+        data['value'].iloc[i] = balance
+data.index = data.index.tz_localize(None)
+
+subset = data[['Close','value']]
+subset.to_excel('output.xlsx')
+
+#subset 2 = Datetime, Close, signal, trd_signal, balance, posit, open_price, value
+subset2 = data[['Close','signal','trd_signal','balance','posit','open_price','value']]
+subset2.to_excel('output2.xlsx')
+
+#plot the data
+plt.plot(subset)
+#legend 
+plt.legend(['Close','Value'])
+plt.title(ticker+" Interval: "+interval+" Period: "+str(period))
+#save the plot
+plt.savefig('plot.png')
+plt.show()
